1dd3c1d3204945706d1a3bfb470c716915769a06
GoreOtUma <marina.5kyshnareva@mail.ru>
2025-04-13 01:36:32 +0300

    Profile Fix

--- frontend/src/components/MainLogo.jsx
+++ frontend/src/components/MainLogo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import imageTmpkLogo from "../assets/tmpk-logo.png";
 import "../styles/MainLogo.css";
 
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/MainPage.css";
 import "../styles/Profile.css";
 import InfoBlock from "../components/InfoBlock.jsx"
+import MyButton from "../components/UI/button/MyButton.jsx"
 import { useNavigate } from "react-router-dom";
 import arrowMore from "../assets/arrow-more.png";
 
@@ -16,10 +17,15 @@ const Profile = () => {
           <span className="main-page__username">Иван Иванович</span>
           <img className="arrow-more" alt="" src={arrowMore} />
         </div>
-        <div className='info-blocks'>
-          <InfoBlock header='Иванов Иван Иванович' body='worker2022@uni-dubna.ru'></InfoBlock>
-          <InfoBlock header='Логин' body='NewWorker22'></InfoBlock>
-          <InfoBlock header='Пароль' body='Password1!'></InfoBlock>
+        <div className='profile-info'>
+          <div className='info-blocks'>
+            <InfoBlock header='Иванов Иван Иванович' body='worker2022@uni-dubna.ru'></InfoBlock>
+            <InfoBlock header='Логин' body='NewWorker22'></InfoBlock>
+            <InfoBlock header='Пароль' body='Password1!'></InfoBlock>
+          </div>
+          <div className='buttons main-buttons'>
+            <MyButton type="button">Выйти</MyButton>
+          </div>
         </div>
       </div>
     );
