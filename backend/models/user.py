d3a50fac734ea7f03930691e6db3181ac362caa0
bvv.21 <bvv.21@uni-dubna.ru>
2025-05-09 06:21:04 +0300

    modify models

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import enum
-from sqlalchemy import String, text, ForeignKey
+from sqlalchemy import String, UniqueConstraint, text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Annotated
 from ..db.session import Base, str_128, str_256, str_64
@@ -52,6 +52,10 @@ class Port(Base):
     status_link:Mapped[LinkStatus] = mapped_column(nullable=False, default=LinkStatus.Disconnected) #ADDD ENUMM
     switch_id:Mapped[int] = mapped_column(ForeignKey("switches.id", ondelete="CASCADE"), nullable=False)
     
+    __table_args__ = (
+        UniqueConstraint('number', 'switch_id', name='port_number_switch_id_key'),
+    )
+    
 class Task(Base):
     __tablename__ = 'tasks'
     
@@ -68,14 +72,14 @@ class Service(Base):
     __tablename__ = 'services'
     
     id:Mapped[intpk]
-    name_service:Mapped[str_256] = mapped_column(nullable=False)
+    name_service:Mapped[str_256] = mapped_column(nullable=False, unique=True)
     
 class Switch(Base):
     __tablename__ = 'switches'
     
     id:Mapped[intpk]
     name_com:Mapped[str_256] = mapped_column(nullable=False)
-    IP:Mapped[str_64] = mapped_column(nullable=False)
+    IP:Mapped[str_64] = mapped_column(nullable=False, unique=True)
     
 class Address(Base):
     __tablename__ = 'addresses'
@@ -89,9 +93,8 @@ class Offer(Base):
     __tablename__ = 'offers'
     
     id:Mapped[intpk]
-    phone:Mapped[str] = mapped_column(String(10), nullable=False, unique=True)
+    phone:Mapped[str] = mapped_column(String(10), nullable=False)
     osmp:Mapped[int] = mapped_column(nullable=False, unique=True)
-    abon_name:Mapped[str_128] = mapped_column(nullable=False)
     date_d:Mapped[datetime] = mapped_column(nullable=False, server_default=text("TIMEZONE('utc', now())"))
     address_id: Mapped[int] = mapped_column(ForeignKey("addresses.id", ondelete="CASCADE"), nullable=False)
     abon_id: Mapped[int] = mapped_column(ForeignKey("abonents.id", ondelete="CASCADE"), nullable=False)
